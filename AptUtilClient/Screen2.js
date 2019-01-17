bb5ac7880604cdc517a5cf183ff422bec0bfd194
FrozenrainYoo <frozenrain.yoo@gmail.com>
2019-01-18 00:57:14 +0900

    add convert UI  m2 to 평

--- AptUtilClient/Screen2.js
+++ AptUtilClient/Screen2.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 type Props = {};
 export default class Screen2 extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { text: '',
+      value: '',
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Screen 2 😎</Text>
+        <Text style={styles.welcome}>Welcome to Screen 2 😎 </Text>
         <Button title="Go to screen 1" onPress={() => this.props.navigation.navigate('Screen1')} />
+
+        <View style={{ flexDirection: 'row' }}>
+          <TextInput
+            keyboardType='number-pad'
+            style={{ flex: 1, height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={(text) => this.setState({ value:text * 0.3025 })}
+          />
+        </View>
+        <Text style={styles.welcome}>{this.state.value}평</Text>
       </View>
     );
   }
